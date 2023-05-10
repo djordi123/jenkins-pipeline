ff6cb512547d236775a07598c66386fd3a4d522b
djordi123 <djordi_1998@hotmail.com>
2023-05-10 15:10:33 +0200

    PHP files added

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="static/css/default-style.css" type="text/css" rel="stylesheet" />
+	<!-- roboto font -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+	<title><?php echo $title ?></title>
+</head>
+
+<body>
+	<header>
+		<!-- navbar -->
+		<nav class="navbar">
+			<!-- title -->
+			<div><a class="title" href="index.php">Jenkins Pipeline</a></div>
+			<!-- navbar buttons -->
+			<div class="navbar-links">
+				<a href="index.php">Home</a>
+				<a href="files.php">Link 1</a>
+				<a href="archive.php">Link 2</a>
+			</div>
+		</nav>
+	</header>
+    <main>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$title = "Jenkins Pipeline";
+require 'header.php';
+?>
+
+<h1>Hello world</h1>
+
+<?php require 'footer.php'; ?>
